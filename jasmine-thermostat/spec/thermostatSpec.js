118de874f4c2360d3a0934cfef92a14cf2d14a6e
Alex Kharouk <akharoukpp@gmail.com>
2018-10-03 15:22:02 +0100

    begin adding html/css

--- jasmine-thermostat/spec/thermostatSpec.js
+++ jasmine-thermostat/spec/thermostatSpec.js
@@ -22,7 +22,7 @@ describe("Thermostat", function() {
   });
 
   it("has a minimum of 10 degrees", function() {
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 11; i++) {
       thermostat.down();
     }
     expect(thermostat.getCurrentTemperature()).toEqual(10);
@@ -35,5 +35,55 @@ describe("Thermostat", function() {
   it("can switch off the PSM", function() {
     thermostat.switchPowerSavingModeOff();
     expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+
+  it("can switch the PSM back on", function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+
+  describe("when power saving mode is on", function() {
+    it("has a max temp of 25 degrees", function() {
+      for (var i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+
+  describe("when power saving mode is off", function() {
+    it("has a max temp of 32 degrees", function() {
+      thermostat.temperature = 32;
+      // thermostat.up();
+      expect(function() {
+        thermostat.up();
+      }).toThrow(new Error("Can't exceed max temp!"));
+    });
+  });
+  describe("it can reset temperature", function() {
+    it("resets temperature", function() {
+      thermostat.up();
+      thermostat.reset();
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe("Gives energy usage", function() {
+    it("Gives low usage when below 18 degrees", function() {
+      thermostat.temperature = 17;
+      expect(thermostat.powerUsage()).toEqual("Low usage");
+    });
+
+    it("Gives mid usage when below 25 degrees", function() {
+      thermostat.temperature = 19;
+      expect(thermostat.powerUsage()).toEqual("Medium usage");
+    });
+
+    it("Gives high usage when above 25 degrees", function() {
+      thermostat.temperature = 26;
+      expect(thermostat.powerUsage()).toEqual("High usage");
+    });
   });
 });
--- jasmine-thermostat/src/thermostat.js
+++ jasmine-thermostat/src/thermostat.js
@@ -4,6 +4,8 @@ function Thermostat() {
   this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
   this.powerSavingMode = true;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -11,7 +13,17 @@ Thermostat.prototype.getCurrentTemperature = function() {
 };
 
 Thermostat.prototype.up = function() {
-  this.temperature += 1;
+  if (this.isMaximumTemperature()) {
+    return;
+  }
+
+  if (this.temperature > this.MAX_LIMIT_PSM_OFF) {
+    throw new Error("Can't exceed max temp!");
+  } else if (this.temperature > this.MAX_LIMIT_PSM_ON) {
+    throw new Error("Can't exceed max temp!");
+  } else {
+    this.temperature += 1;
+  }
 };
 
 Thermostat.prototype.down = function() {
@@ -25,10 +37,32 @@ Thermostat.prototype.isMinimumTemperature = function() {
   return this.temperature === this.MINIMUM_TEMPERATURE;
 };
 
+Thermostat.prototype.isMaximumTemperature = function() {
+  return this.temperature === this.MAX_LIMIT_PSM_ON;
+};
+
 Thermostat.prototype.isPowerSavingModeOn = function() {
   return this.powerSavingMode;
 };
 
 Thermostat.prototype.switchPowerSavingModeOff = function() {
   this.powerSavingMode = false;
+};
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+  this.powerSavingMode = true;
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
+};
+
+Thermostat.prototype.powerUsage = function() {
+  if (this.temperature < 18) {
+    return "Low usage";
+  } else if (this.temperature >= 18 && this.temperature <= 25) {
+    return "Medium usage";
+  } else {
+    return "High usage";
+  }
 };
