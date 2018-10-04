04551859339a281e5bed016e748c57f263b21902
Alex Kharouk <akharoukpp@gmail.com>
2018-10-04 14:45:03 +0100

    add AJAX request for weather api

--- jasmine-thermostat/src/interface.js
+++ jasmine-thermostat/src/interface.js
@@ -45,5 +45,13 @@ $(document).ready(function() {
     $(".PSM").text("Off");
   });
 
-  $("#errorMessage").text("error");
+  // $("#errorMessage").text("error");
+
+  $.get(
+    "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=bd8eea6308feeee2b7bb38e0e09e6795&units=metric",
+    function(temperature) {
+      var result = temperature.main.temp;
+      $("#apiDegree").text(result);
+    }
+  );
 });
